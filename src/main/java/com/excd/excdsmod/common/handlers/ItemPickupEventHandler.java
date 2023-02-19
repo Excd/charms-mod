fc6256d1a0e54e974ee045c9ead4791be5ad60ef
Excd <greggseamon@gmail.com>
2023-02-19 06:27:08 -0500

    Created simple itemPickup() and checkCharms() events

--- src/main/java/com/excd/excdsmod/common/handlers/ItemPickupEventHandler.java
+++ src/main/java/com/excd/excdsmod/common/handlers/ItemPickupEventHandler.java
@@ -1,14 +1,36 @@
 package com.excd.excdsmod.common.handlers;
 
+import com.excd.excdsmod.ExcdsMod;
+import com.excd.excdsmod.init.ItemInit;
+
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.ItemStack;
+import net.minecraftforge.event.TickEvent;
+import net.minecraftforge.event.entity.player.PlayerEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+
 /**
  * @author Greggory Seamon
  */
+@Mod.EventBusSubscriber(modid = ExcdsMod.MODID)
 public final class ItemPickupEventHandler {
 
-	/**
-	 * 
-	 */
-	public ItemPickupEventHandler() {
-		// TODO Auto-generated constructor stub
+	@SubscribeEvent
+	public static void itemPickup(PlayerEvent.ItemPickupEvent event) {
+		ItemStack itemStack = event.getStack();
+		
+		if (itemStack.getItem().equals(ItemInit.WOODEN_CHARM.get())) {
+			ExcdsMod.LOGGER.info("Picked up: " + itemStack.getItem());
+		}
+	}
+	
+	@SubscribeEvent
+	public static void checkCharms(TickEvent.PlayerTickEvent event) {
+		Player player = event.player;
+		
+		if (player.getInventory().contains(new ItemStack(ItemInit.WOODEN_CHARM.get()))) {
+			System.out.println("Inventory contains: " + ItemInit.WOODEN_CHARM.get());
+		}
 	}
 }
