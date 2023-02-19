d90e66f6c35e2b916c834de9686529eed3e36c80
Excd <greggseamon@gmail.com>
2023-02-19 04:11:33 -0500

    Created WoodenCharmItem.java

---
+++ src/main/java/com/excd/excdsmod/common/items/WoodenCharmItem.java
@@ -0,0 +1,13 @@
+package com.excd.excdsmod.common.items;
+
+import net.minecraft.world.item.Item;
+
+/**
+ * @author Greggory Seamon
+ */
+public final class WoodenCharmItem extends Item {
+
+	public WoodenCharmItem(Properties properties) {
+		super(properties);
+	}
+}
