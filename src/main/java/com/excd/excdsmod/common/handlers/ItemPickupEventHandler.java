069ca2c7eefeb9b87daee1628233ae1686168545
Excd <greggseamon@gmail.com>
2023-02-19 05:19:56 -0500

    Created ItemPickupEventHandler.java

---
+++ src/main/java/com/excd/excdsmod/common/handlers/ItemPickupEventHandler.java
@@ -0,0 +1,14 @@
+package com.excd.excdsmod.common.handlers;
+
+/**
+ * @author Greggory Seamon
+ */
+public final class ItemPickupEventHandler {
+
+	/**
+	 * 
+	 */
+	public ItemPickupEventHandler() {
+		// TODO Auto-generated constructor stub
+	}
+}
