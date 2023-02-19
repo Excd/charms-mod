df84bef946bd38081f35d5d8e228b6efd66922a1
Excd <greggseamon@gmail.com>
2023-02-18 19:58:39 -0500

    Created item SMALL_CHARM
    Added item to tools and utilities creative tab.
    Registered ITEMS and ItemInit to mod event bus.
    Registered main class to Forge core event bus.

--- src/main/java/com/excd/excdsmod/ExcdsMod.java
+++ src/main/java/com/excd/excdsmod/ExcdsMod.java
@@ -1,12 +1,12 @@
 package com.excd.excdsmod;
 
+import com.excd.excdsmod.init.ItemInit;
 import com.mojang.logging.LogUtils;
-import net.minecraft.world.level.block.Blocks;
+import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
-import net.minecraftforge.registries.ForgeRegistries;
 import org.slf4j.Logger;
 
 /**
@@ -14,21 +14,24 @@ import org.slf4j.Logger;
  */
 @Mod(ExcdsMod.MODID)
 public class ExcdsMod {
-	
+
     public static final String MODID = "excdsmod";				// Public mod id reference.
-    
+
     private static final Logger LOGGER = LogUtils.getLogger();	// slf4j logger reference.
 
     public ExcdsMod() {
         IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
+        modEventBus.addListener(this::setup);		// Register the setup method for modloading.
+        
+        ItemInit.ITEMS.register(modEventBus);		// Register items to mod event bus.
+        
+        modEventBus.register(ItemInit.class);		// Register ItemInit class to mod event bus.
 
-        // Register the setup method for modloading.
-        modEventBus.addListener(this::setup);
+        MinecraftForge.EVENT_BUS.register(this);	// Register class to core Forge event bus.
     }
 
     private void setup(final FMLCommonSetupEvent event) {
-    	// Some common setup code.
-        LOGGER.info("HELLO FROM COMMON SETUP");
-        LOGGER.info("DIRT BLOCK >> {}", ForgeRegistries.BLOCKS.getKey(Blocks.DIRT));
+    	// Setup code.
+        LOGGER.info("HELLO WORLD!");
     }
 }
--- src/main/java/com/excd/excdsmod/init/ItemInit.java
+++ src/main/java/com/excd/excdsmod/init/ItemInit.java
@@ -2,16 +2,35 @@ package com.excd.excdsmod.init;
 
 import com.excd.excdsmod.ExcdsMod;
 
+import net.minecraft.world.item.CreativeModeTabs;
 import net.minecraft.world.item.Item;
+import net.minecraftforge.event.CreativeModeTabEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
 
 /**
  * @author Greggory Seamon
  */
 public final class ItemInit {
-
+	
 	// Static reference to Item register.
 	public static final DeferredRegister<Item> ITEMS = 
 			DeferredRegister.create(ForgeRegistries.ITEMS, ExcdsMod.MODID);
+	
+	// Register item with name string and supplier object.
+	// Should be final/static and conventionally named in all upper-case with underscores.
+	// String name should be lower-case with underscores.
+	public static final RegistryObject<Item> SMALL_CHARM = ITEMS.register("small_charm",
+			() -> new Item(new Item.Properties()));
+	
+	// Add items to creative inventory tabs.
+	@SubscribeEvent
+	public static void buildContents(CreativeModeTabEvent.BuildContents event) {
+		// Add items to tools and utilities creative tab.
+		if (event.getTab() == CreativeModeTabs.TOOLS_AND_UTILITIES) {
+			event.accept(SMALL_CHARM);
+		}
+	}
 }
