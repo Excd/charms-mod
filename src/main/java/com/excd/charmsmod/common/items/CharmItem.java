c236fa1123d2e1df712fcb6564f482e6060f4b27
Excd <greggseamon@gmail.com>
2023-02-21 04:45:35 -0500

    Removed AttributeModifier related code from CharmItem.java
    Moved to CharmMobEffect.java

--- src/main/java/com/excd/charmsmod/common/items/CharmItem.java
+++ src/main/java/com/excd/charmsmod/common/items/CharmItem.java
@@ -1,8 +1,5 @@
 package com.excd.charmsmod.common.items;
 
-import net.minecraft.world.entity.ai.attributes.AttributeModifier;
-import net.minecraft.world.entity.ai.attributes.Attributes;
-import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.Item;
 
 /**
@@ -12,59 +9,21 @@ public class CharmItem extends Item {
 
 	private boolean modifierApplied;
 	private float healthModifier;
-	private AttributeModifier attributeModifier;
 
 	/**
 	 * @param itemProperties
 	 */
 	protected CharmItem(Item.Properties itemProperties) {
 		super(itemProperties);
-		setHealthModifier(1.0f);
-		setAttributeModifier(new AttributeModifier("MaxHealthModifier", 
-				getHealthModifier(),
-				AttributeModifier.Operation.ADDITION));
+		setHealthModifier(0.0f);
 	}
 
-	public CharmItem applyModifier(Player player) {
-		
-		if (!isModifierApplied()) {
-			setModifierApplied(true);
-			
-			if (!player.getAttribute(Attributes.MAX_HEALTH).hasModifier(getAttributeModifier())) {
-				player.getAttribute(Attributes.MAX_HEALTH).addTransientModifier(getAttributeModifier());
-				System.out.println("Player Max Health: " + player.getMaxHealth());
-			}
-		}
-		
-		return this;
-	}
-	
-	public void removeModifier(Player player) {
-		
-		if (isModifierApplied()) {
-			setModifierApplied(false);
-			
-			if (player.getAttribute(Attributes.MAX_HEALTH).hasModifier(getAttributeModifier())) {
-				player.getAttribute(Attributes.MAX_HEALTH).removeModifier(getAttributeModifier());
-				System.out.println("Player Max Health: " + player.getMaxHealth());
-			}
-		}
-	}
-	
 	public float getHealthModifier() {
 		return healthModifier;
 	}
 
 	public void setHealthModifier(float healthModifier) {
 		this.healthModifier = healthModifier;
-	}
-	
-	public AttributeModifier getAttributeModifier() {
-		return attributeModifier;
-	}
-
-	public void setAttributeModifier(AttributeModifier attributeModifier) {
-		this.attributeModifier = attributeModifier;
 	}
 
 	public boolean isModifierApplied() {
